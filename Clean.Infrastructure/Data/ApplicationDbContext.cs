af9148f23bd1628c9dc2eecd292424400ed25302
henrry <henamorado29102@gmail.com>
2024-07-04 20:47:08 -0400

    add identity

--- Clean.Infrastructure/Data/ApplicationDbContext.cs
+++ Clean.Infrastructure/Data/ApplicationDbContext.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Clean.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Clean.Infrastructure.Data
 {
-   public class ApplicationDbContext : DbContext
-{
-    public DbSet<Product> Products { get; set; }
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
-}
 
 }
